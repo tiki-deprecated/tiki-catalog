e040654c62a802ab46336c2b0d298cc27fa77d0b
Mike Audi <mike@mytiki.com>
2023-01-10 16:18:47 -0600

    syntax fixes

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS address (
     address_aid BYTEA PRIMARY KEY,
     address BYTEA NOT NULL,
     aid TEXT NOT NULL,
-    created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_utc TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 -- -----------------------------------------------------------------------
@@ -18,16 +18,18 @@ CREATE TABLE IF NOT EXISTS address (
 -- -----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS block (
     block_hash BYTEA PRIMARY KEY,
-    address_aid BYTEA FOREIGN KEY address (address_aid),
+    address_aid BYTEA NOT NULL,
     src_url TEXT NOT NULL,
     created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
+    FOREIGN KEY(address_aid) REFERENCES address(address_aid)
 );
 
 -- -----------------------------------------------------------------------
 -- TRANSACTION
 -- -----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS txn (
-   txn_hash BYTEA PRIMARY KEY,
-   block_hash BYTEA FOREIGN KEY block (block_hash),
-   created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
+    txn_hash BYTEA PRIMARY KEY,
+    block_hash BYTEA NOT NULL,
+    created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
+    FOREIGN KEY(block_hash) REFERENCES block(block_hash)
 );
